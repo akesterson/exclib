b233356ad3f1fe0c0d1114435f6a6575624a919c
Andrew Kesterson <andrew@aklabs.net>
2011-07-27 22:36:01 -0400

    Removed backtrace.h

--- backtrace.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BACKTRACE_H__
-#define __BACKTRACE_H__
-
-extern int backtrace(void **buffer, int size);
-extern char **backtrace_symbols(void *const *buffer, int size);
-
-#endif // __BACKTRACE_H__
